42a3f785775e3b3cf75be698369bc4870365ace1
servicebog <iandre@gdbnetwork.com>
2025-08-03 21:03:14 +0300

    Update

--- runtime.lua
+++ runtime.lua
@@ -3,6 +3,7 @@ local HttpService = game:GetService("HttpService")
 
 local headers = {
     ["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    ["Content-Type"] = "application/json",
     ["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
 }
 
