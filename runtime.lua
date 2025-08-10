8688b5b2620b01b4ab2be0ad2ad752ab034953dd
servicebog <iandre@gdbnetwork.com>
2025-08-10 14:42:17 +0300

    U

--- runtime.lua
+++ runtime.lua
@@ -53,7 +53,8 @@ while game.PlaceId == 142823291 or game.PlaceId == 335132309 or game.PlaceId == 
             Headers = headers
         })
     
-    print(response.Body)
+    print("Ping response:")
+    print(HttpService:JSONEncode(response.Body))
 
     wait(60)
 end
