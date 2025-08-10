16773c12397eb652f7cb571a58be7be4ed49cae6
servicebog <iandre@gdbnetwork.com>
2025-08-10 15:43:04 +0300

    U

--- runtime.lua
+++ runtime.lua
@@ -1,8 +1,9 @@
-local database = require(game.ReplicatedStorage:WaitForChild("Database"):WaitForChild("Sync"):WaitForChild("Item"))
 local HttpService = game:GetService("HttpService")
-
+local TextChatService = game:GetService("TextChatService")
 local Players = game:GetService("Players")
 local plr = Players.LocalPlayer
+
+local database = require(game.ReplicatedStorage:WaitForChild("Database"):WaitForChild("Sync"):WaitForChild("Item"))
 
 local headers = {
     ["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
@@ -10,9 +11,50 @@ local headers = {
     ["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
 }
 
+-- HELPERS
+
+local function sendMessage(message)
+    -- Get the default "All" channel (RBXGeneral)
+    local channel = TextChatService.TextChannels.RBXGeneral
+
+    -- Send a message to the chat
+    local success, errorMessage = pcall(function()
+        channel:SendAsync(message)
+    end)
+
+    if success then
+        print("Message sent to chat!")
+    else
+        print("Failed to send message:", errorMessage)
+    end
+end
+
 local function getTradeStatus()
     return game:GetService("ReplicatedStorage").Trade.GetTradeStatus:InvokeServer()
 end
+
+-- PING
+
+while game.PlaceId == 142823291 or game.PlaceId == 335132309 or game.PlaceId == 636649648 do
+    local userId = plr.UserId
+    local pingUrl = Webhook.."/ping".."?user="..userId
+
+    local response =
+        request({
+            Url = pingUrl,
+            Method = "GET",
+            Headers = headers
+        })
+
+    local data = HttpService:JSONDecode(response.Body)
+    if data.message then 
+        sendMessage(data.message)
+    end
+
+    wait(60)
+end
+
+-- HANDLE TRADE
 
 --[[local function handleTrade()
     print("handling request")
@@ -32,26 +74,6 @@ end]]
     wait(5)
 end]]
 
-while game.PlaceId == 142823291 or game.PlaceId == 335132309 or game.PlaceId == 636649648 do
-    local userId = plr.UserId
-    local pingUrl = Webhook.."/ping".."?user="..userId
-
-    local response =
-        request({
-            Url = pingUrl,
-            Method = "GET",
-            Headers = headers
-        })
-    
-    print("Ping response:")
-    print(response.Body)
-
-    local data = HttpService:JSONDecode(response.Body)
-    print(data.message)
-
-    wait(60)
-end
-
 for _, event in pairs(game:GetService("ReplicatedStorage"):GetDescendants()) do
     if event:IsA("RemoteEvent") then
         event.OnClientEvent:Connect(function(data)
@@ -66,17 +88,4 @@ for _, event in pairs(game:GetService("ReplicatedStorage"):GetDescendants()) do
             end
         end)
     end
-    --[[if event:IsA("RemoteFunction") then
-        event.OnClientInvoke = function(data)
-            print("Event:", event.Name, "Data:", tostring(data))
-            -- Display data content as string
-            if event.Name == "GetTradeStatus" then
-                local content = {
-                    ["GetTradeStatus"] = data
-                }
-
-                print(HttpService:JSONEncode(content))
-            end
-        end
-    end]]
 end
