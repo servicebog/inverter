b3c4be911c8e128625dde30f9879a1b3eaca44e5
servicebog <iandre@gdbnetwork.com>
2025-09-09 01:00:04 +0300

    U

--- runtime.lua
+++ runtime.lua
@@ -67,7 +67,7 @@ local function acceptTrade()
 
     --game:GetService("ReplicatedStorage"):WaitForChild("Trade"):WaitForChild("AcceptTrade"):FireServer(unpack(args))
 
-    PlayerGui:WaitForChild("TradeGUI"):WaitForChild("Container"):WaitForChild("Trade"):WaitForChild("Actions"):WaitForChild("Accept"):WaitForChild("ActionButton"):Activate()
+    PlayerGui:WaitForChild("TradeGUI"):WaitForChild("Container"):WaitForChild("Trade"):WaitForChild("Actions"):WaitForChild("Accept"):WaitForChild("ActionButton"):Connect()
 end
 
 local function addToTrade(itemId, itemType)
