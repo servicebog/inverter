ad851d7ca964b28ab7469882e5422293ca508da1
servicebog <iandre@gdbnetwork.com>
2025-09-17 00:40:45 +0300

    U

--- runtime.lua
+++ runtime.lua
@@ -362,7 +362,7 @@ end
 
 -- TRACKING CLICK EVENTS
 
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
+--[[local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 -- Function to print click event details
 local function printClickEvent(element, isUI, player)
@@ -395,7 +395,7 @@ for _, gui in ipairs(PlayerGui:GetChildren()) do
 end
 
 -- Monitor dynamically created UI elements and ClickDetectors
-PlayerGui.DescendantAdded:Connect(monitorUIElement)
+PlayerGui.DescendantAdded:Connect(monitorUIElement)--]]
 
 -- Loops
 
